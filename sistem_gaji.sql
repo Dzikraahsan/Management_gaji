34e8addb88911ccdbcdc001bd0455f63739d5d0f
Dzikra Ahsan Imawan <ahsandzikra@gmail.com>
2025-06-06 15:15:49 +0700

    mengubah kode

--- sistem_gaji.sql
+++ sistem_gaji.sql
@@ -11,10 +11,15 @@ CREATE TABLE jabatan (
 CREATE TABLE karyawan (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nama VARCHAR(100) NOT NULL,
+  umur INT NOT NULL,
+  jenis_kelamin enum('Laki-laki', 'Perempuan', '', '')
   alamat TEXT,
   no_hp VARCHAR(20),
+  status_ VARCHAR NOT NULL,
   jabatan_id INT,
   foto VARCHAR(255),
+  nilai_rating INT NOT NULL,
+  tgl_bergabung DATE NOT NULL,
   FOREIGN KEY (jabatan_id) REFERENCES jabatan(id)
 );
 
@@ -28,7 +33,9 @@ CREATE TABLE rating (
 
 CREATE TABLE lembur (
   id INT AUTO_INCREMENT PRIMARY KEY,
+  jabatan_id INT,
   karyawan_id INT,
+  nama_jabatan VARCHAR(100),
   bulan VARCHAR(20),
   jam_lembur INT,
   tarif_per_jam DECIMAL(15,2),
@@ -38,6 +45,10 @@ CREATE TABLE lembur (
 CREATE TABLE gaji (
   id INT AUTO_INCREMENT PRIMARY KEY,
   karyawan_id INT,
+  nama_karyawan VARCHAR(64),
+  gaji_pokok INT(16),
+  tarif_lembur INT(16),
+  bonus_rating INT(16),
   bulan VARCHAR(20),
   total_gaji DECIMAL(15,2),
   FOREIGN KEY (karyawan_id) REFERENCES karyawan(id)
